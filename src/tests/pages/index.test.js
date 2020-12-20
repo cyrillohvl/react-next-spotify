7c01ea3092acb72637dc2bdd07fe8f18cd3a7017
Patrick Coutinho <patrick@istyle.com.br>
2020-12-20 19:11:16 -0300

    added a basic test

--- src/tests/pages/index.test.js
+++ src/tests/pages/index.test.js
@@ -5,16 +5,6 @@ import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Home from '../../pages/index';
 
-const server = setupServer(
-  rest.get('/', (req, res, ctx) => {
-    return res(ctx.json({ greeting: 'hello there' }));
-  }),
-);
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
-
 test('loads and displays index', async () => {
   render(<Home />);
   expect(screen.getByTestId('login')).toBeInTheDocument();
