1b81e7b5e7059f072deb559fa0f6faf04b8f9921
Patrick Coutinho <patrick@istyle.com.br>
2020-12-20 21:05:48 -0300

    Removed unnused imports

--- src/tests/pages/index.test.js
+++ src/tests/pages/index.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
-import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Home from '../../pages/index';
 
